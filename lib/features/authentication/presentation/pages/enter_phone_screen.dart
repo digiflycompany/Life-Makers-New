020cefabe5ee1971a78a0a993790759c9d000bbb
20192726 <20192726@fue.edu.eg>
2024-03-07 11:56:46 +0200

    Enter Confirmation Code Text

--- lib/features/authentication/presentation/pages/enter_phone_screen.dart
+++ lib/features/authentication/presentation/pages/enter_phone_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:life_makers/core/utils/app_routes.dart';
 import 'package:life_makers/core/utils/extensions.dart';
 import 'package:life_makers/core/widgets/custom_snack_bar.dart';
 import 'package:life_makers/features/authentication/cubit/sign_up_cubit/sign_up_cubit.dart';
 import 'package:life_makers/features/authentication/cubit/sign_up_cubit/sign_up_states.dart';
+import 'package:life_makers/features/authentication/presentation/widgets/enter_phone_widgets/enter_confirmation_code_text.dart';
 import 'package:life_makers/features/authentication/presentation/widgets/enter_phone_widgets/enter_phone_image.dart';
 import 'package:life_makers/features/authentication/presentation/widgets/enter_phone_widgets/forgot_password_text.dart';
 import '../../../../core/utils/app-assets.dart';
@@ -47,49 +47,38 @@ class EnterPhoneScreen extends StatelessWidget {
                   children: [
                     const EnterPhoneImage(),
                     const ForgotPasswordText(),
-                    enterConfirmationCodeText,
-                    SizedBox(height: 40.h,),
-                    Padding(
-                      padding:  EdgeInsets.symmetric(horizontal: 24.w),
-                      child: Column(
-                        children: [
-                          phoneTextField,
-                          SizedBox(height: 75.h),
-                          Padding(
-                              padding:  EdgeInsets.symmetric(horizontal: 7.w),
-                              child: GestureDetector(
-                                onTap: (){
-                                  if(_formKey.currentState!.validate()){
-                                    signUpCubit.OtpResetPasswordCheck(signUpCubit.phoneResetPasswordController.text);
-                                  }
-                                },
-                                child: Container(
-                                  width: 420.w,
-                                  height: 63.h,
-                                  decoration: BoxDecoration(
-                                    color: AppColors.orangeBorderColor,
-                                    borderRadius: BorderRadius.circular(8.r),
-                                  ),
-                                  child:  Center(
-                                    child: signUpCubit.phoneResetPasswordLoading == true ?Transform.scale(
-                                      scale: 0.4,
-                                      child: CircularProgressIndicator(
-                                        color: AppColors.white,
-                                      ),
-                                    ):Text(
-                                      AppStrings.send,
-                                      style: const TextStyle(
-                                          color: Colors.white,
-                                          fontFamily: FontFamilies.alexandria,
-                                          fontWeight: FontWeight.w500,
-                                          fontSize: 12
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              )
+                    const EnterConfirmationCodeText(),
+                    phoneTextField,
+                    SizedBox(height: 75.h),
+                    GestureDetector(
+                      onTap: (){
+                        if(_formKey.currentState!.validate()){
+                          signUpCubit.OtpResetPasswordCheck(signUpCubit.phoneResetPasswordController.text);
+                        }
+                      },
+                      child: Container(
+                        width: 420.w,
+                        height: 63.h,
+                        decoration: BoxDecoration(
+                          color: AppColors.orangeBorderColor,
+                          borderRadius: BorderRadius.circular(8.r),
+                        ),
+                        child:  Center(
+                          child: signUpCubit.phoneResetPasswordLoading == true ?Transform.scale(
+                            scale: 0.4,
+                            child: CircularProgressIndicator(
+                              color: AppColors.white,
+                            ),
+                          ):Text(
+                            AppStrings.send,
+                            style: const TextStyle(
+                                color: Colors.white,
+                                fontFamily: FontFamilies.alexandria,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12
+                            ),
                           ),
-                        ],
+                        ),
                       ),
                     ),
                   ],
@@ -102,35 +91,6 @@ class EnterPhoneScreen extends StatelessWidget {
     ),
 );
   }
-  get createNewPassword => Text(
-    AppStrings.createNewPassowrd,
-    textDirection: TextDirection.rtl,
-    style: TextStyle(
-        color: AppColors.gradientColor1,
-        fontWeight: FontWeight.w500,
-        fontSize: 13.5
-    ),
-  );
-  get confirmationCodeText => Text(
-    AppStrings.forgetPasswordQuestion,
-    textDirection: TextDirection.rtl,
-    style: TextStyle(
-        color: AppColors.gradientColor1,
-        fontWeight: FontWeight.w500,
-        fontFamily: FontFamilies.alexandria,
-        fontSize: 13.5
-    ),
-  );
-  get enterConfirmationCodeText => Text(
-    AppStrings.enterPhoneNumberForConfirmation,
-    textDirection: TextDirection.rtl,
-    style: TextStyle(
-        color: AppColors.smallTextColor,
-        fontWeight: FontWeight.w400,
-        fontFamily: FontFamilies.alexandria,
-        fontSize: 10.8
-    ),
-  );
   get phoneTextField => RegularTextField(
     max: 11,
     readOnly: false,
