a3dafd43861a38bdd24f6da0e3e099fd430838c9
20192726 <20192726@fue.edu.eg>
2024-03-11 11:38:54 +0200

    Code Not Sent Text

--- lib/features/authentication/presentation/pages/otp_screen.dart
+++ lib/features/authentication/presentation/pages/otp_screen.dart
@@ -12,7 +12,7 @@ import 'package:life_makers/features/authentication/cubit/sign_up_cubit/sign_up_
 import 'package:life_makers/features/authentication/cubit/sign_up_cubit/sign_up_states.dart';
 import 'package:life_makers/features/authentication/data/models/phone_user_mode.dart';
 import 'package:life_makers/features/authentication/presentation/pages/change_password.dart';
-import 'package:life_makers/features/authentication/presentation/pages/enter_phone_screen.dart';
+import 'package:life_makers/features/authentication/presentation/widgets/otp_widgets/code_not_sent_text.dart';
 import 'package:life_makers/features/authentication/presentation/widgets/otp_widgets/code_will_be_sent_text.dart';
 import 'package:life_makers/features/authentication/presentation/widgets/otp_widgets/otp_image.dart';
 import 'package:life_makers/features/authentication/presentation/widgets/otp_widgets/otp_text_field.dart';
@@ -68,39 +68,7 @@ class OtpScreen extends StatelessWidget {
                     const ForgetPasswordText(),
                     const CodeWillBeSentText(),
                     const OtpCustomTextField(),
-                    SizedBox(height: 55.h),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      textDirection: TextDirection.rtl,
-                      children: [
-                        didNotReceiveOtp,
-                        SizedBox(width: 3.w,),
-                        GestureDetector(
-                          onTap: (){
-                            Navigator.pushReplacement(context, PageTransition(
-                              type: PageTransitionType.fade,
-                              duration: const Duration(milliseconds: 400),
-                              child:  BlocProvider.value(
-                                value: context.read<SignUpCubit>(),
-                                child:EnterPhoneScreen(),
-                              ),
-                            ),
-                            );
-                          },
-                          child: Text(
-                            AppStrings.resendOtp2,
-                            textDirection: TextDirection.rtl,
-                            style: TextStyle(
-                                color: AppColors.gradientColor1,
-                                fontWeight: FontWeight.w600,
-                                fontFamily: FontFamilies.alexandria,
-                                fontSize: 10.8
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 30.h),
+                    const CodeNotSentText(),
                     Padding(
                         padding:  EdgeInsets.symmetric(horizontal: 25.w),
                         child:GestureDetector(
@@ -164,14 +132,4 @@ class OtpScreen extends StatelessWidget {
     ),
 );
   }
-  get didNotReceiveOtp =>Text(
-    AppStrings.didNotReceiveOtp,
-    textDirection: TextDirection.rtl,
-    style: TextStyle(
-        color: AppColors.smallTextColor,
-        fontWeight: FontWeight.w400,
-        fontFamily: FontFamilies.alexandria,
-        fontSize: 10.8
-    ),
-  );
 }
