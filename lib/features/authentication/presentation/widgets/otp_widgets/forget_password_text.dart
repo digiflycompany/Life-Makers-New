d6a84cfd2a2697633a05034c70c04cc7fc36cc72
20192726 <20192726@fue.edu.eg>
2024-03-11 11:03:57 +0200

    Forget Password Text

---
+++ lib/features/authentication/presentation/widgets/otp_widgets/forget_password_text.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:life_makers/core/utils/app-color.dart';
+import 'package:life_makers/core/utils/app-string.dart';
+import 'package:life_makers/core/utils/extensions.dart';
+
+class ForgetPasswordText extends StatelessWidget {
+  const ForgetPasswordText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: context.height20),
+      child: Text(
+        AppStrings.forgetPasswordQuestion,
+        textDirection: TextDirection.rtl,
+        style: TextStyle(
+            color: AppColors.gradientColor1,
+            fontWeight: FontWeight.w500,
+            fontSize: 13.5.sp
+        ),
+      ),
+    );
+  }
+}
