6d49948020aa6211f2cd8e883359b7c58885d75a
20192726 <20192726@fue.edu.eg>
2024-03-12 10:59:53 +0200

    Change Password Button

--- lib/features/authentication/presentation/pages/change_password.dart
+++ lib/features/authentication/presentation/pages/change_password.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:life_makers/features/authentication/cubit/sign_up_cubit/sign_up_cubit.dart';
 import 'package:life_makers/features/authentication/cubit/sign_up_cubit/sign_up_states.dart';
 import 'package:life_makers/features/authentication/presentation/pages/login_screen.dart';
-import 'package:life_makers/features/authentication/presentation/widgets/change_password_widgets/change_passsword_image.dart';
+import 'package:life_makers/features/authentication/presentation/widgets/change_password_widgets/change_password_image.dart';
+import 'package:life_makers/features/authentication/presentation/widgets/change_password_widgets/change_password_button.dart';
 import 'package:life_makers/features/authentication/presentation/widgets/change_password_widgets/create_new_password_text.dart';
 import 'package:life_makers/features/authentication/presentation/widgets/change_password_widgets/reset_passwords_text_fields.dart';
 import 'package:page_transition/page_transition.dart';
-import '../../../../core/utils/app-color.dart';
 import '../../../../core/utils/app-string.dart';
-import '../../../../core/utils/app_fonts.dart';
 
 class ChangePasswordScreen extends StatefulWidget {
   ChangePasswordScreen({super.key});
@@ -61,45 +59,7 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                       const ChangePasswordImage(),
                       const CreateNewPasswordText(),
                       const ResetPasswordsTextFields(),
-                      SizedBox(
-                        height: 30.h,
-                      ),
-                      Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 25.w),
-                          child: GestureDetector(
-                            onTap: () {
-                              if (_formKey.currentState!.validate()) {
-                                signUpCubit.ChangePassword(
-                                    signUpCubit.resetPasswordController.text,
-                                    signUpCubit.confirmResetPasswordController.text);
-                              }
-                            },
-                            child: Container(
-                              width: 420.w,
-                              height: 63.h,
-                              decoration: BoxDecoration(
-                                color: AppColors.orangeBorderColor,
-                                borderRadius: BorderRadius.circular(8.r),
-                              ),
-                              child: Center(
-                                child: state is changePasswordLoading
-                                    ? Transform.scale(
-                                  scale: 0.4,
-                                  child: CircularProgressIndicator(
-                                    color: AppColors.white,
-                                  ),
-                                )
-                                    : Text(
-                                  AppStrings.confirm,
-                                  style: const TextStyle(
-                                      color: Colors.white,
-                                      fontFamily: FontFamilies.alexandria,
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 12),
-                                ),
-                              ),
-                            ),
-                          )),
+                      ChangePasswordButton(formKey: _formKey,),
                     ],
                   ),
                 ),
